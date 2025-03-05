cd9db8ca5b52d916b4c7f42d893b60205fb6ce45
pigalleg <pigalleg@gmail.com>
2025-03-05 10:30:20 +0000

    In SUC, removed LOL and GEN costs from OPEX expression and accounted explicitly for these terms in the objective function via a model[:penalizationCosts] term

--- model/post_processing.jl
+++ model/post_processing.jl
@@ -342,7 +342,7 @@ function get_enriched_objective_value(enriched_solution, gen_df, storage, parame
     #TODO: deal with missing values
     function check()
         aux = combine(groupby(cost, intersect([:scenario], propertynames(cost))), [:production_cost, :fixed_cost, :start_cost] .=> (x -> sum(skipmissing(x))), renamecols = false)
-        sum_cost = mean(aux.production_cost.+.+aux.fixed_cost.+aux.start_cost)
+        sum_cost = mean(aux.production_cost.+aux.fixed_cost.+aux.start_cost)
         if !isapprox(enriched_solution[:scalar].OPEX[1], sum_cost; rtol =  parameters.MIPGap) # OPEX = production_cost + fixed_cost + start_cost
             error("Start and operational cost missmatch with OPEX")
         end
