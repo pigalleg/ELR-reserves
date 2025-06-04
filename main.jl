c01aaa8c92cfcdf7dc294d91c2a0085778ee45a5
Pablo Gallegos <pigalleg@gmail.com>
2025-06-04 11:21:44 +0100

    Bug on reserve_repartition corrected Minor modifications

--- main.jl
+++ main.jl
@@ -154,11 +154,11 @@ function generate_post_processing_KPI_files(folder_path; stochastic = false, fol
         # Check consistency in objective function
         x = sort(gcdi_KPI_adequacy, group_by)
         y = sort(gcdi_objective_function_KPI, group_by)
-        if !(all(isapprox.(x.objective_value,  sum(eachcol(y[:,intersect([:OPEX, :LOL_cost, :LGEN_cost, :reserve_cost], propertynames(y))])), rtol=10^-8)))
-            error("Missmatch in objective value")
+        if !(all(isapprox.(x.objective_value,  sum(eachcol(y[:,intersect([:OPEX, :LOL_cost, :LGEN_cost, :reserve_cost, :slack_reserve_up_cost, :slack_reserve_down_cost], propertynames(y))])), rtol=10^-8)))
+            error("Mismatch in objective value")
         end
         if !(all(isapprox.(x.OPEX,  y.OPEX , rtol=10^-8)))
-            error("Missmatch in OPEX")
+            error("Mismatch in OPEX")
         end
     end
 
