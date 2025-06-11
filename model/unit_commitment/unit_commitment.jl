706197779dc4c2acea0c17c56d89e0695ce87f20
Pablo Gallegos <pigalleg@gmail.com>
2025-06-11 15:52:18 +0100

    corrected flag for storage reserve repartition

--- model/unit_commitment/unit_commitment.jl
+++ model/unit_commitment/unit_commitment.jl
@@ -8,7 +8,7 @@ function construct_deterministic_unit_commitment(gen_df, loads, gen_variable, mi
     energy_reserve = get(kwargs, :energy_reserve, nothing)
     storage_envelopes = get(kwargs, :storage_envelopes, true)
     storage_link_constraint =  get(kwargs, :storage_link_constraint, false)
-    storage_reserve_repartition =  get(kwargs, :storage_reserve_repartition, -1) # -1 means no repartitioning, 0 means no reserve for storage, and any other positive number is the percentage of the reserve that should be allocated to storage
+    storage_reserve_repartition =  get(kwargs, :storage_reserve_repartition, 1) # -1 means no repartitioning, 0 means no reserve for storage, and any other positive number is the percentage of the reserve that should be allocated to storage
     VRESERVE = get(kwargs, :VRESERVE, 1e-6)
     VSRESUP = get(kwargs, :VSRESUP, 1e4)
     VSRESDN = get(kwargs, :VSRESDN, 30)
