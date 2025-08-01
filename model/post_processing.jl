99918e88a6bb2743fb4236290582412970ddc968
Pablo Gallegos <pablo.gallegos@imperial.ac.uk>
2025-08-01 12:01:43 +0100

    New benchmarks created. Extra cost removed.

--- model/post_processing.jl
+++ model/post_processing.jl
@@ -603,8 +603,8 @@ function get_enriched_objective_value(enriched_solution, gen_df, storage, parame
         select!(losses_cost, Not(fields_to_remove))
         cost = vcat(cost, losses_cost, cols=:union) 
     end
-    # check_cost_consistency()
-    @warn "Cost consistency not checked" 
+    check_cost_consistency()
+    # @warn "Cost consistency not checked" 
     return cost
 end
 
